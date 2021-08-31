1f909e61531fa186434ddc91be8288627174969d
lastdefiance20 <lastdefiance20@gmail.com>
2021-08-31 23:05:29 +0900

    register updated

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//login
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+const {User} = require("./models/User");
+
+app.post('/api/users/register', (req, res) => {
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
 //URL 연결
 app.get('/', (req, res) => {
   res.send('Hello World!')
